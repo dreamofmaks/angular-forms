1d0334c63885d6a11c457eb520e627aba7902f02
Maksym Leontiev <mleont@softserveinc.com>
2021-02-28 13:08:12 +0200

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      surname: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      address: new FormGroup({
+        country: new FormControl('ua'),
+        city: new FormControl('', Validators.required)
+      })
+    });
+  }
+
+  Submit() {
+    if(this.form.valid) {
+      console.log('Form: ', this.form);
+      const formData = {...this.form.value};
+      console.log("form data:", formData)
+    }
+  }
+}
